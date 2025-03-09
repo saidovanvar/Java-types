2f4e46dacb9f94342dcfed5995458e5eb6c3f6cf
saidovanvar <saidovanvar@gmail.com>
2025-03-09 10:28:16 +0500

    Anvar safari

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+// let name = "hello"
+// let message = 98
+// let messages = 9n
+// let result = true
+//
+// console.log(name, message, messages, result)
 
-let name = "hello"
-let message = 98
-let messages = 9n
-let result = true
-
-console.log(name, message, messages, result)
+let nem = prompt("Hamma harajatlar 9732738.6 so'm")
+console.log(nem)
+if (nem>9732738.6){
+    console.log("Oq yul,Anvar!")
+}else {
+    console.log("Anvar,ozgina sabr qilish kerak bular ekan")
+}
