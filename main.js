88703fcecf485eea2f2a809ace84c7dc83bd6830
saidovanvar <saidovanvar@gmail.com>
2025-06-08 21:01:45 +0500

    new project

--- main.js
+++ main.js
@@ -1,28 +1,64 @@
 let li = document.querySelector("#list")
 let dataFetch = async () => {
-    let status = await fetch("https://crm-test-api.duckdns.org/api/Leads", {
-        headers: {
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyIiwibmFtZSI6ImFzaWxiZWsiLCJ1bmlxdWVfbmFtZSI6Iis5OTg5MTQ1NDIzMzkiLCJqdGkiOiIwN2NjZWFjZi0zNzYyLTRhOGYtOTVkMi1jZDA3NjhmNzBjM2IiLCJUZW5hbnRJZCI6IjEiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJBZG1pbiIsImV4cCI6MTc1NTA4NDE5OSwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAwIn0.UFF5AqUWqO-Reh-bDURJ4Vz1ifmbiu6zE47Rbzp_M-g"
-        }
+    li.innerHTML = ''
+    let status = await fetch("https://effective-mobile.duckdns.org/api/ads/", {
     });
     let data = await status.json();
-    getData(data.items);
+    getData(data.results);
 }
 dataFetch();
 
 function getData(data ) {
-    data.forEach((item , index) => {
+    data.slice(0 ,8).forEach((item, index) => {
         console.log(item)
         let list = document.createElement("li");
+        li.style.height = "500px";
         list.innerHTML = `
-        <div class="bg-neutral-950 text-[#fff] rounded-[25px] text-center p-4 m-auto">
+        <div style="background: #fff700;
+background: linear-gradient(93deg, rgba(255, 247, 0, 1) 0%, rgba(255, 0, 0, 1) 100%);"
+        class="text-[#fff] rounded-[25px] text-center p-4 max-w-[300px] h-[250px]">
         <p>
-        <span>${index+1}</span>
-        ${item.firstName} {item.lastName}
+        <span>${index + 1}</span>
+        ${item.title}
         </p>
-        <p>${item.phoneNumber}</p>
+        <p>${item.description.length > 60 ? item.description.slice(0, 60) + "..." : item.description}</p>
+        
+        <img class="w-[110px] h-[150px] m-auto rounded-[20px]" src="${item.image_url ? item.image_url : "./iphone.jpg"}" alt="${item.image_url }">
         <div>`;
         list.style.listStyle = "none";
-        li.append(list);
+        li.appendChild(list);
     })
 }
+
+    let form = document.querySelector("#form");
+    let title = document.querySelector("#title");
+    let description = document.querySelector("#description");
+    let img_url = document.querySelector("#image_url");
+    let category = document.querySelector("#category");
+    let country = document.querySelector("#condition");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("submit");
+    let postData = async () => {
+        let post = await fetch("https://effective-mobile.duckdns.org/api/ads/", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                title: title.value,
+                description: description.value,
+                image_url: img_url.value,
+                category: category.value,
+                condition: country.value,
+            })
+        })
+        if (post.ok) {
+            alert("ishlayapdi")
+        }else{
+            alert("error");
+        }
+
+    }
+    postData()
+    dataFetch();
+})
